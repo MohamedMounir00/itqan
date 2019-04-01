930c0f72397977f629ca6810d3da495a36fa8905
Mohammed Mounir <m.mounir@sprints.ws>
2019-04-01 09:57:07 +0200

    v88

---
+++ app/Http/Controllers/Backend/NotificationsController.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\NotificationBackent;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Carbon\Carbon;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+use Yajra\Datatables\Datatables;
+use Alert;
+use DB;
+class NotificationsController extends Controller
+{
+    //
+
+    public function index()
+    {
+
+
+        return view('notifications.notify');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+
+        return view('');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(DataRequest $request)
+    {
+
+
+    }
+
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+
+
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(DataRequest $request, $id)
+    {
+
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $data = NotificationBackent::findOrFail($id);
+
+        $data->delete();
+        Alert::success(trans('backend.deleteFlash'))->persistent("Close");
+
+        return response()->json([
+            'success' => 'Record has been deleted successfully!'
+        ]);
+    }
+
+
+
+
+    public function getAnyDate()
+    {
+        $data = NotificationBackent::orderBy('created_at','DESC');
+
+        return Datatables::of($data)
+            ->addColumn('action', function ($data) {
+                return '
+              <button class="btn btn-delete btn btn-round  btn-danger" data-remote="notifications/' . $data->id . '"><i class="fa fa-remove"></i>'.trans('backend.delete').'</button>
+              
+    
+                ';
+            })
+            ->addColumn('message', function ($data) {
+                return '<a href="' . route('order.show', $data->order_id) . '">'.unserialize($data->message)[LaravelLocalization::getCurrentLocale()].'</a>';
+
+
+            })
+            ->addColumn('created_at', function ($data) {
+                $language = LaravelLocalization::getCurrentLocale();
+
+                Carbon::setLocale($language);
+                    return  Carbon::parse($data->created_at)->diffForHumans();
+
+
+
+            })
+
+            ->rawColumns(['action', 'message'])
+            ->make(true);
+    }
+
+
+}
