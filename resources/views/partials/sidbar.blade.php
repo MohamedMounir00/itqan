a25e19820dfce9d160c3e9bba5be6d018c3f52a1
Mohammed Mounir <m.mounir@sprints.ws>
2019-04-07 11:18:50 +0200

    v89

--- resources/views/partials/sidbar.blade.php
+++ resources/views/partials/sidbar.blade.php
@@ -27,7 +27,6 @@
         <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
             <div class="menu_section">
                 <ul class="nav side-menu">
-{{--
 
                     <li><a><i class="fa fa-edit"></i> {{trans('backend.technical_controller')}} <span class="fa fa-chevron-down"></span></a>
                         <ul class="nav child_menu">
@@ -142,7 +141,7 @@
                         </ul>
                       </li>
 
---}}
+
                     <li><a href="{{route('holidays.index')}}"><i class="fa fa-edit"></i> {{trans('backend.day')}} <span class="fa fa-chevron-down"></span></a>
 
                     </li>
