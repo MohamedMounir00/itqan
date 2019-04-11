27f464349ecad5c2ee0e125d2ba04789edc9b1ed
Mohammed Mounir <m.mounir@sprints.ws>
2019-04-11 12:56:47 +0200

    v152

--- resources/views/time/index.blade.php
+++ resources/views/time/index.blade.php
@@ -9,8 +9,7 @@
                 <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                 </li>
 
-                <li><a class="close-link"><i class="fa fa-close"></i></a>
-                </li>
+
 
                 <li><a   href="{{route('time_work.create')}}" class=""><i class="fa fa-plus-square"></i></a>
                 </li>
